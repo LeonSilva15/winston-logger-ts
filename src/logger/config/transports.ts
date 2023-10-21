6e1a44e7b9978c6e897b5269253b430572dc3792
LeonSilva15 <l30silva.o@gmail.com>
2023-10-21 17:50:01 -0600

    feat: add default transports handling

--- src/logger/config/transports.ts
+++ src/logger/config/transports.ts
@@ -1,7 +1,24 @@
-import { transports } from 'winston';
+import winston, { transports } from 'winston';
+import { defaults } from './';
 
-export const DEFAULTS = {
-    'transports': [
-        new transports.Console()
-    ]
-};
+export default class Transports {
+    public static addDefaultTransports( transport: string, logger: winston.Logger ) {
+        switch( transport ) {
+            case 'Console':
+                logger.add(
+                    new transports.Console({
+                        format: defaults.console.format,
+                    })   
+                );
+                break;
+            case 'File':
+                logger.add(
+                    new transports.File({
+                        format: defaults.console.file.format,
+                        filename: defaults.console.file.filename,
+                    })   
+                );
+                break;
+        }
+    }
+}
